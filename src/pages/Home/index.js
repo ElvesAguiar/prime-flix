89860f98492ccfcbc8f99a7cd29a87d28581119e
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-14 13:48:09 -0300

    feat: using hooks make http GET request

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,28 @@
 
+import { useEffect, useState } from "react";
+ import api from '../../services/api'
+
+//URL DA API: movie/now_playing?api_key=3976d2a147b98c9113783fca3aee342c&language=pt-BR
+
 function Home(){
+const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+    async function loadFilmes(){
+      const reponse = await api.get("movie/now_playing?api_key=3976d2a147b98c9113783fca3aee342c&language=pt-BR",{
+        params:{
+          api_key:"3976d2a147b98c9113783fca3aee342c",
+          language: "pt-BR",
+          page :1
+        }
+      }
+  )
+      console.log(reponse.data.results);
+}
+
+    loadFilmes();
+  },[])
+
     return(
       <div>
         <h1>BEM VINDO A HOME</h1>
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,13 @@
+import axios from "axios";
+
 //BASE DA API: https://api.themoviedb.org/3/
 
 //URL DA API: movie/now_playing?api_key=3976d2a147b98c9113783fca3aee342c&language=pt-BR
  
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+ 
 
+
+export default api;
